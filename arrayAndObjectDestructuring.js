cb05fc3f54250c5eb136f38ec77a053be6363268
avyrie <pittman.ally@gmail.com>
2020-11-16 13:41:12 -0700

    JavaScript array methods and array/object destructuring

---
+++ arrayAndObjectDestructuring.js
@@ -0,0 +1,66 @@
+// *** Destructureing arrays ***
+
+const words = ['This', 'That', 'The Other', 'Those', 'Them', 'There']
+const alphabet = ['A', 'B', 'C', 'D', 'E', 'F']
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// let [first item, second item, third item, fourth item, fifth item] = array
+let [a, b] = words;
+console.log(a)
+console.log(b)
+
+// ,, means to skip the item in the next position
+let [f,, h] = words;
+console.log(f)
+console.log(h)
+
+let [r, l,, k] = words;
+console.log(r)
+console.log(l)
+console.log(k)
+
+// Spread operator
+let [ q, w, ...rest] = words;
+console.log(q)
+console.log(w)
+console.log(rest)
+
+// Combines arrays
+const newArray = [...alphabet, ...numbers]
+console.log(newArray)
+// Can also use .concat to do the same thing
+const newArr = alphabet.concat(numbers)
+console.log(newArr)
+
+sumAndMultiply = (a, b) => ([a+b, a*b, a/b])
+// sumAndMultiply = (a, b) => ([a+b, a*b])
+
+// if there were no a/b and we console.log(division) the output would be No division
+const [sum, multiply, division = 'No division'] = sumAndMultiply(2, 3)
+console.log(sum)
+console.log(multiply)
+console.log(division)
+
+
+
+
+// *** Destructuring objects ***
+
+const personTwo = {
+    name: 'Sally',
+    age: 32,
+    address: {
+        city: 'Fairax',
+        state: 'Virginia'
+    }
+}
+// const { name, age } = personTwo;
+// console.log(name)
+
+// Can reassign variable (ex. changing 'name' to 'firstName')
+const { name: firstName, age } = personTwo;
+console.log(firstName)
+
+
+
+// Thank you to: https://www.youtube.com/watch?v=NIq3qLaHCIs
---
+++ arrayMethods.js
@@ -0,0 +1,80 @@
+const items = [
+    {name: 'Bike', price: 100},
+    {name: 'Tv', price: 200},
+    {name: 'Album', price: 10},
+    {name: 'Book', price: 5},
+    {name: 'Phone', price: 500},
+    {name: 'Computer', price: 1000},
+    {name: 'Keyboard', price: 25}
+];
+
+// *** Filter ***
+const filteredItems = items.filter((item) => {
+    return item.price <= 100
+})
+console.log(filteredItems)
+// * creates an array of items that meet the specified statement
+
+
+
+// *** Map ***
+const mappedItems = items.map((item) => {
+    return item.name
+})
+console.log(mappedItems)
+// * creates an array of just those values specified
+
+
+
+// *** Find ***
+const foundItem = items.find((item) => {
+    return item.name === 'Book'
+})
+console.log(foundItem)
+// * will return first item it finds in an array that returns true for the statement you pass inside the .find function
+
+
+
+// *** For Each ***
+items.forEach((item) => {
+    console.log(item.name)
+})
+// * does something for every single item in the array
+
+
+
+// *** Some ***
+const cheapItems = items.some((item) => {
+    return item.price <= 100
+})
+console.log(cheapItems)
+// * checks if ANY items in the array return true for the specified statement and returns true if yes, false if no
+
+
+
+// *** Every ***
+const cheapItems2 = items.every((item) => {
+    return item.price <= 100
+})
+console.log(cheapItems2)
+// checks if EVERY item in the array returns true for the specified statement and returns true if yes, false if no
+
+
+
+// ** Reduce ***
+const totalPrice = items.reduce((currentTotal, item) => {
+    return item.price + currentTotal
+}, 0)
+console.log(totalPrice)
+// currentTotal = property that we want to reduce everything into - the total after each iteration of the array - first parameter of function is whatever the previous iteration of the array returned
+// item is each actual item in the array
+// 0 is the starting point
+
+
+
+// *** Includes ***
+const newItems = [1, 2, 3, 4, 5, "six"]
+// const newItems = [1, 2, 3, 4, 5]
+const includesTwo = newItems.includes(2)
+console.log(includesTwo)
+// * checks to see if the array being passed includes the argument, returns true if yes
